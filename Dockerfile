8ed0a2abd4b8a3e2024b37a64cedcc9ebedd75b8
wupeiqun <wupeiqun@baidu.com>
2021-04-22 16:32:56 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM centos
+
+ADD  target/*.jar  /app.jar
+
+EXPOSE  8080
+
+ENTRYPOINT ["java", "-jar", "/app.jar"]
---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/api")
+public class TestController {
+    // -- this is annote
+    @RequestMapping(value = "/my",method = RequestMethod.GET)
+    public String helloView() {
+        return "hello";
+    }
+}
